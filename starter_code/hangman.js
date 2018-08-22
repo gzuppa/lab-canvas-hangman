fd258bb07830ca5c767fa897c5f8e31c287ca97c
gzuppa <gustavo_zuppa_flores@hotmail.com>
2018-08-21 22:13:58 -0500

    primer_commit

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,20 +1,31 @@
 var hangman;
 
-// function Hangman() {
 
-// }
+function Hangman() {
+  this.words = ["lion","turtle","dolphin","rhinoceros","elephant","giraffe","gorilla","octopus","moose"];
+  this.secretWord = "";
+  this.letters = [];
+  this.keyCode = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90];
+  this.guessedLetter = "";
+  this.errorsLeft = 11;
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  return this.secretWord = "";
+};
 
-// };
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  console.log(keyCode)
+  if (keyCode >= 65 && keyCode <= 90){
+    return true
+   }
+   return false  
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkClickedLetters = function (key) { //revisar charcode, indexOf, includes
+  console.log(key)
 
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
+};
 
 // Hangman.prototype.addCorrectLetter = function (i) {
 
